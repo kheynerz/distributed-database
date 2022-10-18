35f5ee408069abb0222d187a711f61440e737b0d
kheynerz <sebastiancr44@gmail.com>
2022-10-17 23:04:30 -0600

    VIEW FINISHED

--- connection.py
+++ connection.py
@@ -1,3 +1,4 @@
+from re import S
 import psycopg2
 from operator import itemgetter
 
@@ -66,10 +67,10 @@ def createTable(name: str, node: dict) -> bool:
 
     return(_execute(getParams(node['name']), sql))
 
-def createForeignTable(name, node, serverName):
-    attributes = node['attributes']
+def createForeignTable(name, node, server):
+    attributes = server['attributes']
 
-    serverName = '_'.join(serverName.lower().split()) 
+    serverName = '_'.join(server['name'].lower().split()) 
     sql =  f"create foreign table remote_{name}_{serverName} ("
 
     for a in attributes:
@@ -77,28 +78,72 @@ def createForeignTable(name, node, serverName):
         sql += f"{atrName} {atrType}, "
 
     sql = sql[0:-2] + f") server {serverName}_postgres_fdw OPTIONS (schema_name 'public', table_name '{name}');"
-      
+    
     return(_execute(getParams(node['name']), sql))
+
+
+def createView(attributes, tables, name):
+
+    view_sql = f"CREATE OR REPLACE VIEW view_{name} AS SELECT "# * from {name} "
+
+    central, locals = itemgetter('central', 'locals')(attributes)
+
+    allAtrs = central['attributes'].copy()
+
+    def checkAtr(atr, attributtes = allAtrs):
+        for a in attributtes:
+            if a['name'] == atr['name']:
+                return True
+        return False    
+
+    for node in locals:
+        atrs = node['attributes']
+        for a in atrs:
+            if not checkAtr(a):
+                allAtrs.append(a)
+    
+    central_cols = ""
+
+    for col in allAtrs:
+        if checkAtr(col, attributtes=central['attributes']):
+            central_cols += col['name'] + '::text, '
+        else:
+            central_cols += 'Null as ' + col['name'] + ', '
+       
+
+    view_sql += central_cols[0:-2] + f" from {name}"
+
+    for node in locals:
+        view_sql += " UNION SELECT "
+        serverName = '_'.join(node['name'].lower().split()) 
+    
+        for col in allAtrs:
+            if checkAtr(col, attributtes=node['attributes']):
+                view_sql += col['name'] + '::text, '
+            else:
+                view_sql += 'Null as ' + col['name'] + ', '
+        view_sql = view_sql[0:-2] + f" from remote_{name}_{serverName}"   
+
+    _execute(getParams(central['name']), view_sql)
+
 
 
 def generateTables(attributes : dict) -> bool:
     name, central, locals = itemgetter('name', 'central', 'locals')(attributes)
 
     #CREATE TABLE FOR CENTRAL NODE
-    #createTable(name, central)
+    createTable(name, central)
 
-    view_sql = f"CREATE OR REPLACE VIEW view_{name} AS SELECT * from {name} "
+    tables = []
 
     #CREATE TABLE FOR LOCALS
     for node in locals:
-        serverName = '_'.join(node['name'].lower().split()) 
-        view_sql += f"UNION SELECT * FROM remote_{name}_{serverName} "
+        tables.append(node['name'])
         createTable(name, node)
-        createForeignTable(name,node, central['name'])
-        createForeignTable(name,central, node['name'])
+        createForeignTable(name,node, central)
+        createForeignTable(name,central, node)
 
-    _execute(getParams(central['name']), view_sql)
-    
+    createView(attributes, tables, name)    
 
 #Postgresql Connection
 #params = {"host" : "localhost", "database" : "postgres", 'port': 5435, "user" : "postgres", "password" : "1234"}
@@ -160,6 +205,36 @@ tabla = {
 #createServer(getParams('INSTANCIA 2'), 'INSTANCIA 1')
 #createServer(getParams('INSTANCIA 3'), 'INSTANCIA 1')
 
-#generateTables(tabla)
+
+tablaHorizontal = {
+    "name": "hori",
+    "central": 
+        {
+            "name": "INSTANCIA 1", 
+            "attributes": [
+                {"name": "cedula", "type" : "varchar", "pk" : True, "null" : False },
+                {"name": "nombre", "type" : "varchar", "pk" : False, "null" : False },
+                {"name": "ape1", "type" : "varchar", "pk" : False, "null" : False }
+            ]
+        },
+    "locals": [
+        {
+            "name": "INSTANCIA 2", 
+            "attributes":[
+                {"name": "cedula", "type" : "varchar", "pk" : True, "null" : False },
+                {"name": "ape2", "type" : "varchar", "pk" : False, "null" : False }
+            ]
+        },
+    
+        {
+            "name": "INSTANCIA 3", 
+            "attributes":[
+                {"name": "cedula", "type" : "varchar", "pk" : True, "null" : False },
+                {"name": "edad", "type" : "int", "pk" : False, "null" : False }
+            ]
+        }
+    ]
+}
 
 
+generateTables(tablaHorizontal)
